57456f008bcae8b9296f1da04870da9a16d6c7e9
clwater <gengzhibo@gengzhibo.net>
2017-08-05 02:45:51 +0800

    20170805

--- src/PushFile.py
+++ src/PushFile.py
@@ -18,9 +18,10 @@ def pushFile(url):
         key = f.read()
 
     print b
-    url = 'ffmpeg -re -i "' + url + '" -vcodec copy -acodec aac -b:a 192k -f flv "rtmp://txy.live-send.acg.tv/live-txy/?streamname=' + streamname+ '&key=' + key
+    url = 'ffmpeg -re -i "' + url + '" -vcodec copy -acodec aac -b:a 192k -f flv "rtmp://txy.live-send.acg.tv/live-txy/?streamname=' + \
+          streamname+ '&key=' + key + '"'
 
 
     print url
     a,b = commands.getstatusoutput(url)
-    # print b
+    print b

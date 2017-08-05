d954e2d3a94306982c3fa332998b532a2d98a849
clwater <gengzhibo@gengzhibo.net>
2017-08-05 13:47:31 +0800

    20170805

--- src/PushFile.py
+++ src/PushFile.py
@@ -20,7 +20,7 @@ def pushFile(url):
     with open('key', 'r') as f:
         key = f.read()
 
-    print b
+    # print b
     url = 'ffmpeg -re -i "/media/pi/smp/xz/file/' + url + '" -vcodec copy -acodec aac -b:a 192k -f flv "rtmp://txy.live-send.acg.tv/live-txy/?streamname=' + \
           streamname+ '&key=' + key + '"'
 

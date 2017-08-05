725c7dd07b020989caf5ec0dc30c73a3f9dab16d
clwater <gengzhibo@gengzhibo.net>
2017-08-05 12:47:37 +0800

    20170805

--- src/ScanFile.py
+++ src/ScanFile.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
 import os
 from os import listdir
 from os.path import isfile, join
 
 def scan():
-    url = '/media/pi/smp/xz/file'
-
+    # url = '/media/pi/smp/xz/file'
+    url = '/Users/yszsyf/Desktop/'
 
     onlyfiles = [f for f in listdir(url) if isfile(join(url, f))]
 
